8b5b36e075392aec0c19332e5d453d9f9b261903
Lucas PRANEUF <lucas.praneuf@gmail.com>
2018-01-14 00:04:17 +0100

    Binary graphics

---
+++ src/main/BinaryManager.java
@@ -0,0 +1,81 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BinaryManager {
+
+	private static final int FIGURE_MAX_VALUE = 9;
+	private static final int BINARY_MAX_DIGIT_FOR_FIGURE = 4;
+
+	private int hoursUnity;
+	private int minutesDecade;
+	private int minutesUnity;
+	private int secondsDecade;
+	private int secondsUnity;
+	private int tenthSeconds;
+
+	BinaryManager(int seconds, int tenthSeconds) {
+		this.hoursUnity = calculateUnity(seconds / 3600);
+		this.minutesDecade = calculateDecade(seconds / 60);
+		this.minutesUnity = calculateUnity(seconds / 60);
+		this.secondsDecade = calculateDecade(seconds);
+		this.secondsUnity = calculateUnity(seconds);
+		this.tenthSeconds = tenthSeconds;
+	}
+
+	private int calculateUnity(int number) {
+		return number % 10;
+	}
+
+	private int calculateDecade(int number) {
+		return calculateUnity(number / 10);
+	}
+
+	public List<Boolean> getListOfBoolean(TimeUnit unit) {
+		List<Boolean> result = new ArrayList<>();
+		switch (unit) {
+			case HOURS_UNITY:
+				result = convertBinaryToListOfBoolean(hoursUnity);
+				break;
+			case MINUTES_DECADE:
+				result = convertBinaryToListOfBoolean(minutesDecade);
+				break;
+			case MINUTES_UNITY:
+				result = convertBinaryToListOfBoolean(minutesUnity);
+				break;
+			case SECONDES_DECADE:
+				result = convertBinaryToListOfBoolean(secondsDecade);
+				break;
+			case SECONDS_UNITY:
+				result = convertBinaryToListOfBoolean(secondsUnity);
+				break;
+			case TENTH_SECONDES:
+				result = convertBinaryToListOfBoolean(tenthSeconds);
+				break;
+		}
+
+		Collections.reverse(result);
+		return result;
+	}
+
+	protected List<Boolean> convertBinaryToListOfBoolean(int number) {
+		if (number > FIGURE_MAX_VALUE) {
+			throw new NotAFigureException(number);
+		}
+
+		List<Boolean> list = new ArrayList<>();
+
+		String binaryString = Integer.toBinaryString(number);
+		for (int i = binaryString.length() - 1; i >= 0; i--) {
+			list.add(Integer.valueOf("" + binaryString.charAt(i)).equals(1));
+		}
+
+		while (list.size() < BINARY_MAX_DIGIT_FOR_FIGURE) {
+			list.add(Boolean.FALSE);
+		}
+
+		return list;
+	}
+}
